cecc43ae1d299d691ce95ae5e7d7521058983e5a
AlTsao <al.tsao.yw@gmail.com>
2021-06-03 23:05:54 +0800

    javascript all play btn

--- script.js
+++ script.js
@@ -42,7 +42,6 @@ const sounds = [
 // load song list 
 let soundContainer = `<h1>BAR SOUND LIST</h1>`;
 
-
 sounds.forEach((sound) => {
   soundContainer += `
     <div class="sound-container">
@@ -59,6 +58,10 @@ sounds.forEach((sound) => {
     </div>
   `
 })
+soundContainer += `
+                    <i id="pause-all" class="far fa-pause-circle"></i>
+                    <i id="pause-all-disabled" class="far fa-pause-circle disabled"></i>
+                  `
 spreadEvenContainer.innerHTML = soundContainer
 
 // add DOM
@@ -67,20 +70,50 @@ let volumeDownJsNames = document.querySelectorAll('#volume-down');
 let audioJsNames = document.querySelectorAll('#audio');
 let soundBarJsNames = document.querySelectorAll('#sound-bar');
 let volumeUpJsNames = document.querySelectorAll('#volume-up');
+let pauseAll = document.getElementById('pause-all');
+let pauseAllDisabled = document.getElementById('pause-all-disabled');
+
+// playing song list
+let playingList = [];
+let playingListAll = [];
+let activeSound = 0;
+let activeSoundAll = 0;
 
 // event listener-play sound
 playJsNames.forEach((item, index) => {
   item.addEventListener('click', () => {
+    playingListAll = [];
+    activeSoundAll = 0;
     if (item.classList.contains('fa-play')) {
-      console.log('fa-play')
       item.classList.replace('fa-play', 'fa-pause');
       item.setAttribute('title', 'Pause');
       audioJsNames[index].play();
       audioJsNames[index].loop = true;
-    } else {
+      // reset last one for all play btn use
+      if (activeSound === 0) {
+        playingList = []
+      };
+      playingList.push(index);
+      activeSound += 1;
+      // delete double index 
+      playingList = playingList.filter((element, index, arr) => {
+        return arr.indexOf(element) === index;
+      });
+      pauseAll.classList.replace('fa-play-circle', 'fa-pause-circle');
+      pauseOrPlayAllSelected();
+    } else if (item.classList.contains('fa-pause')) {
       item.classList.replace('fa-pause', 'fa-play');
       item.setAttribute('title', 'Play');
       audioJsNames[index].pause();
+      activeSound -= 1;
+      // remove pause sounds only remain the last one for all play btn use
+      if (playingList.length > 1) {
+        playingList = playingList.filter(item => item !== index);
+      } else {
+        pauseAll.classList.replace('fa-pause-circle', 'fa-play-circle');
+        playingListAll = [...playingList];
+      }
+      pauseOrPlayAllSelected()
     };
   })
 })
@@ -96,7 +129,7 @@ soundBarJsNames.forEach((item, index) => {
   })
 })
 
-// Sound volume up and down
+// event listener-sound volume up and down
 volumeUpJsNames.forEach((item, index) => {
   item.addEventListener('click', () => {
     soundBarJsNames[index].value++;
@@ -118,3 +151,39 @@ volumeDownJsNames.forEach((item, index) => {
     console.log(audioJsNames[index].volume)
   })
 })
+
+// event listener-Pause all sounds
+pauseAll.addEventListener('click', () => {
+  if (pauseAll.classList.contains('fa-pause-circle')) {
+    playingListAll = [...playingList];
+    activeSoundAll = activeSound;
+    playingList = [];
+    activeSound = 0;
+    audioJsNames.forEach((item, index) => {
+      audioJsNames[index].pause();
+      playJsNames[index].classList.replace('fa-pause', 'fa-play');
+      pauseAll.classList.replace('fa-pause-circle', 'fa-play-circle');
+    })
+  } else if (pauseAll.classList.contains('fa-play-circle')) {
+    playingListAll.forEach((item, index) => {
+      playingList = [...playingListAll];
+      activeSound = activeSoundAll;
+      audioJsNames[item].play()
+      playJsNames[item].classList.replace('fa-play', 'fa-pause');
+      pauseAll.classList.replace('fa-play-circle', 'fa-pause-circle');
+    })
+  }
+
+})
+
+// Check pause or play all available or not
+function pauseOrPlayAllSelected() {
+  if (playingList.length === 0) {
+    pauseAll.style.display = 'none';
+    pauseAllDisabled.style.display = 'block';
+  } else {
+    pauseAll.style.display = 'block';
+    pauseAllDisabled.style.display = 'none';
+  }
+};
+pauseOrPlayAllSelected()
